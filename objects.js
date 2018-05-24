35c8da4f62e81cbb962e6977baf517606471e40e
Matthew Smyth <msmyth1030@gmail.com>
2018-05-24 21:20:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = {dave: 'satellite'};
 function updatePlaylist(playlist, artist, title) {
-  return Object.assign(playlist, { [artist]: title });
+  playlist[artist] = song;
+  return playlist;
 }
 function removeFromPlaylist(playlist, artist) {
   delete playlist[artist];
